b0fad7dc33f32a3f7f73822084ea1dd9bcc94971
Hunter <153857391+7Hunter7@users.noreply.github.com>
2024-06-22 10:34:01 +0300

    Added Vue.js_lesson-4 --> Calc
    Добавлен калькулятор:
    --> "Урок 7. Лекция - Vue CLI"
    --> Курс "Фреймворк Vue.js"

---
+++ 1-Calc/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import CalcComponent from "./components/CalcComponent.vue";
+</script>
+
+<template>
+  <div>
+    <h2>Калькулятор</h2>
+    <CalcComponent />
+  </div>
+</template>
+
+<style>
+h2 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ 1-Calc/src/components/CalcComponent.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div class="calc">
+      <input class="calc__input" type="text" v-model="result" />
+      <button
+        class="calc__buttons"
+        @click="addButton(num)"
+        v-for="num in numbers"
+        :key="num.id"
+      >
+        {{ num }}
+      </button>
+      <button
+        class="calc__buttons"
+        @click="addButton(tab)"
+        v-for="tab in tabs"
+        :key="tab.id"
+      >
+        {{ tab }}
+      </button>
+      <button class="calc__buttons" @click="calcResult">=</button>
+      <button class="button_reset" @click="reset">CE</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CalcComponent",
+
+  data() {
+    return {
+      result: "",
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+      tabs: ["-", "+", "*", "/"],
+    };
+  },
+
+  methods: {
+    addButton(char) {
+      this.result = this.result.toString();
+      this.result += char;
+    },
+    calcResult() {
+      this.result = eval(this.result);
+    },
+    reset() {
+      this.result = "";
+    },
+  },
+};
+</script>
+
+<style>
+.calc {
+  display: grid;
+  grid-template-columns: repeat(3, 100px);
+  justify-content: center;
+  gap: 10px;
+}
+.calc__input {
+  grid-column: 1/-1;
+  margin-bottom: 10px;
+  text-align: right;
+}
+
+.button_reset {
+  grid-column: 1/-1;
+}
+</style>
