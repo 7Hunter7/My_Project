3868e3401a12ac77dae7be967d3bf4683d548f1a
Hunter <153857391+7Hunter7@users.noreply.github.com>
2024-06-23 19:20:25 +0300

    Added GaleryComponent
    Добавлено решение заданий:
    --> семинара "Урок 8. Семинар: Vue cli"
    --> по курсу "Фреймворк Vue.js"

---
+++ 1-Galery/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import GaleryComponent from "./components/GaleryComponent.vue";
+</script>
+
+<template>
+  <div>
+    <h2>Галерея</h2>
+    <GaleryComponent />
+  </div>
+</template>
+
+<style>
+h2 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ 1-Galery/components/GaleryComponent.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div>
+      <img class="dig__img" :src="currentImgUrl" :alt="currentImgAlt" />
+    </div>
+    <div>
+      <img
+        class="small__img"
+        v-for="(img, index) in imgList"
+        :key="index"
+        :src="img.url"
+        :alt="img.alt"
+        @click="selectImgIndex(index)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GaleryComponent",
+  data() {
+    return {
+      currentImgIndex: 0,
+
+      imgList: [
+        {
+          url: "https://avatars.mds.yandex.net/i?id=1cf04a6f38f0be15415a0c35010d27a3b2bac0ff-12496338-images-thumbs&n=13",
+          alt: "Wallpapers",
+        },
+        {
+          url: "https://avatars.mds.yandex.net/i?id=017409fc1a69d562293799b116a3eab70f0366bc-12394646-images-thumbs&n=13",
+          alt: "Cityscape",
+        },
+        {
+          url: "https://avatars.mds.yandex.net/i?id=0e72934b679a07922fd2daa9314658620b56a30e-10143230-images-thumbs&n=13",
+          alt: "National park",
+        },
+        {
+          url: "https://avatars.mds.yandex.net/i?id=7072d8ce023d40f3ed7ff5bec0ddd06f06fae2bd-12884924-images-thumbs&n=13",
+          alt: "Sea",
+        },
+        {
+          url: "https://avatars.mds.yandex.net/i?id=445ba0741950817243e4378a0620e4b973a63f8d-12496338-images-thumbs&n=13",
+          alt: "Summer",
+        },
+      ],
+    };
+  },
+  methods: {
+    selectImgIndex(index) {
+      this.currentImgIndex = index;
+    },
+  },
+  computed: {
+    currentImgUrl() {
+      return this.imgList[this.currentImgIndex].url;
+    },
+    currentImgAlt() {
+      return this.imgList[this.currentImgIndex].alt;
+    },
+  },
+};
+</script>
+<style>
+.small__img {
+  width: 100px;
+}
+.dig__img {
+  width: 300px;
+}
+</style>
